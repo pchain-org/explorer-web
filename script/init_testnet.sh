bdc9eb2d51000ede3c0dad4f9314e2b18c30705b
nieben <nieben201@hotmail.com>
2023-06-02 14:43:38 +0800

    debug

--- script/init_testnet.sh
+++ script/init_testnet.sh
@@ -2,6 +2,6 @@
 git pull origin dev
 sudo yarn install
 sudo yarn build:test
-cp -r ./script/ecosystem.config.js.testnet ecosystem.config.js
+cp -n ./script/ecosystem.config.js.testnet ecosystem.config.js
 pm2 start ecosystem.config.js
 echo "testnet init done"
